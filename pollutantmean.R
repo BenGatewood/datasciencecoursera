c95fefe2939e52e93a1cd30bfbf37e14a07e5a06
Ben Gatewood <ben@Bens-MacBook-Pro.local>
2016-12-19 19:37:58 +0000

    Old changes

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,3 +1,8 @@
 pollutantmean <- function(directory, pollutant, id = 1:332) {
-  
+        
+        for(i in id) {
+          csvFile <- paste(getwd(), "/", directory, "/", i, ".csv", sep = "")
+          print(csvFile)
+        }
+        
 }
