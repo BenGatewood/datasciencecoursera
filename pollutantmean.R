fdf9e8f74c3c5dc30b5aad3bae770a3bd678865e
Ben <ben@gatewood.org.uk>
2016-12-19 11:49:49 +0000

    Finally in a fucking data.frame()

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,3 +1,19 @@
 pollutantmean <- function(directory, pollutant, id = 1:332) {
   
+        files <- list.files(directory)
+        csvList <- list()
+        colNames <- c("Date","sulfate","nitrate","ID")
+        
+        for(f in files[id]) {
+          csv <- read.csv(paste(getwd(), "/", directory, "/", f, sep = ""),
+                          na.strings = "NA")
+          csvList[[f]] <- csv
+
+        }
+        
+         
+    dataFrame <- data.frame(Reduce(rbind, csvList))
+    
+    print(dataFrame)
+  
 }
